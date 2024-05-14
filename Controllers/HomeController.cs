37350094a549df8bb4f5f181c5e8005d12a7d119
BT-ICD <icd.bhavin@gmail.com>
2024-05-14 13:30:44 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+using karnaCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace karnaCrud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<UserModel> userList = new List<UserModel>();
+        private static int nextId = 0;
+        public IActionResult Index()
+        {
+            return View(userList);
+        }
+        public IActionResult Form() //Form 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(UserModel user)
+        {
+            if(ModelState.IsValid)
+            {
+                user.ID = ++nextId;
+                if (user.ID != 0)
+                {
+                    var existingUser = userList.FirstOrDefault(u => u.ID == user.ID);
+                    if (existingUser != null)
+                    {
+                        existingUser.Name = user.Name;
+                        existingUser.Email = user.Email;
+                        existingUser.Phone = user.Phone;
+                        existingUser.Gender = user.Gender;
+                        existingUser.Address = user.Address;
+                        return RedirectToAction("Index");
+                    }
+                }
+                userList.Add(user);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Form", user);
+            }
+            
+        }
+        public IActionResult Delete(int id)
+        {
+
+            var userToDelete = userList.FirstOrDefault(u => u.ID == id);
+            if (userToDelete != null)
+            {
+                userList.Remove(userToDelete);
+            }
+            return RedirectToAction("Index");
+
+        }
+        
+        public IActionResult Update(UserModel model)
+        {
+            var existingUser = userList.FirstOrDefault(u => u.ID == model.ID);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+            existingUser.Name = model.Name;
+            existingUser.Email = model.Email;
+            existingUser.Phone = model.Phone;
+            existingUser.Gender = model.Gender;
+            existingUser.Address = model.Address;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace karnaCrud.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "Only alphabetic characters are allowed in the name")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
+        [MaxLength(10, ErrorMessage = "Phone number must be at most 10 characters long")]
+        [MinLength(10, ErrorMessage = "Phone number must be at least 10 characters long")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please select one gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(1000, ErrorMessage = "Address cannot exceed 1000 characters")]
+        public string Address { get; set; }
+    }
+}
